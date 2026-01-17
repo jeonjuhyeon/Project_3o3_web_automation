982c696000c884f475d0e6232ed40cc785055273
jeonjuhyeon <jhjeon0207@gmail.com>
2026-01-17 10:47:35 +0900

    update conftest : ci

--- conftest.py
+++ conftest.py
@@ -1,11 +1,19 @@
+import os
 import pytest
 from playwright.sync_api import sync_playwright
 
 @pytest.fixture
-def page(request):
+def page():
+    is_ci = os.getenv("CI") == "true"
+
     with sync_playwright() as p:
-        browser = p.chromium.launch(headless=False, slow_mo=500)
-        page = browser.new_page()
+        browser = p.chromium.launch(
+        headless=is_ci,   
+        slow_mo=0 if is_ci else 500
+    )
+        
+        context = browser.new_context()
+        page = context.new_page()
 
         yield page
         browser.close()
